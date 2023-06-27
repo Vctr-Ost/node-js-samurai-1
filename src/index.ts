2db6826c59241759862350c4c51ea16c8ab47c7e
Viktor Ostapenko <ostapenko.vctr@gmail.com>
2023-06-27 20:38:32 +0300

    fix port (new)

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.get('/', (req: any, res: any) => {
-    let msg = 'aaaHisss!';
+    let msg = 'aaaHisssasasasasa!';
     res.send(msg)
 })
 
